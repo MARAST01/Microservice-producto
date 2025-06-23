38b6cbbf285141c483e902b612a841bab7637cb0
lauracoicue <coicue.laura@correounivalle.edu.co>
2025-06-22 23:25:56 -0500

    correcion de nombre de ProductControllerTestU

--- src/test/java/com/products/product/pruebas_Unitarias/ProductControllerTestU.java
+++ src/test/java/com/products/product/pruebas_Unitarias/ProductControllerTestU.java
@@ -26,7 +26,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class ProductControllerTest {
+class ProductControllerTestU {
 
     @Mock
     private ProductService productService;

e2887fd7d12656857f4991a8b21713cc9a8d4962
lauracoicue <coicue.laura@correounivalle.edu.co>
2025-06-23 00:21:55 -0500

    Monitoreo con Spring Boot Admin

---
+++ src/main/java/com/products/product/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.products.product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll() // 👉 Permite libre acceso
+                        .anyRequest().permitAll() // o `.authenticated()` si luego agregas JWT
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
+
